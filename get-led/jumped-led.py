a6b0ee8ed13ff509cf78a64e945774dd5049795e
DaMwY5 <shar.dam655v@gmail.com>
2025-09-16 16:47:07 +0300

    s

--- get-led/jumped-led.py
+++ get-led/jumped-led.py
@@ -2,19 +2,32 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-leds = [24,22,23,27,17,25,12,16]
+leds = [16,12,25,17,27,23,22,24]
 
+n = 0
 GPIO.setup(leds,GPIO.OUT)
-
+up = 9
+down = 10
+GPIO.setup(up,GPIO.IN)
+GPIO.setup(down,GPIO.IN)
 GPIO.output(leds,0)
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2::].zfill(8)]
+sleep_time = 0.2
+while True:
+    if GPIO.input(up):
+       n += 1 
+       if n > 255:
+        n = 255
+    print(n,dec2bin(n))
+    time.sleep(sleep_time)
+    if GPIO.input(down):
+       n -= 1 
+       if n < 0:
+        n = 0
+    print(n,dec2bin(n))
+    time.sleep(sleep_time)
+    b = dec2bin(n)
+    for i in range(8):
+        GPIO.output(leds[i],b[i])
 
-light_time = 0.2
-
-for led in reversed(leds):
-    GPIO.output(led,1)
-    time.sleep(light_time)
-    GPIO.output(led,0)
-for led in leds:
-    GPIO.output(led,1)
-    time.sleep(light_time)
-    GPIO.output(led,0)
