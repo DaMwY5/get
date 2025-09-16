b9e882fdde7fcac41860b0fa8f18b9dd6b8d086e
DaMwY5 <shar.dam655v@gmail.com>
2025-09-16 15:59:06 +0300

    попытка

---
+++ get-led/auto-light.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+led = 26
+GPIO.setup(led,GPIO.OUT)
+state = 0
+period = 0.5
+port = 6
+GPIO.setup(port,GPIO.IN)
+while True:
+    input_state = GPIO.input(port)
+    GPIO.output(led,not input_state)
